075279467f26acb908a19a756b6f2e1475be9fa4
vaneIv <appvane@gmail.com>
2023-06-03 18:47:18 +0200

    Wrote the needed test classes and some tests for the ShoppingItemViewModel

--- app/src/main/java/com/example/shoppinglisttestingexample/other/Constants.kt
+++ app/src/main/java/com/example/shoppinglisttestingexample/other/Constants.kt
@@ -3,7 +3,7 @@ package com.example.shoppinglisttestingexample.other
 object Constants {
 
     const val DATABASE_NAME = "shopping_db"
-    const val BASE_URL = "https://pixabay.com"
+    const val BASE_URL = "https://api.unsplash.com/"
 
     const val MAX_NAME_LENGTH = 20
     const val MAX_PRICE_LENGTH = 10
--- app/src/main/java/com/example/shoppinglisttestingexample/ui/ShoppingViewModel.kt
+++ app/src/main/java/com/example/shoppinglisttestingexample/ui/ShoppingViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.shoppinglisttestingexample.data.local.ShoppingItem
 import com.example.shoppinglisttestingexample.data.remote.responses.UnsplashResponse
+import com.example.shoppinglisttestingexample.other.Constants
 import com.example.shoppinglisttestingexample.other.Event
 import com.example.shoppinglisttestingexample.other.Resource
 import com.example.shoppinglisttestingexample.repositories.ShoppingRepository
@@ -26,10 +27,11 @@ class ShoppingViewModel @Inject constructor(
     val images: LiveData<Event<Resource<UnsplashResponse>>> = _images
 
     private val _curImageUrl = MutableLiveData<String>()
-    val curImageUrl:LiveData<String> = _curImageUrl
+    val curImageUrl: LiveData<String> = _curImageUrl
 
     private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
-    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> = _insertShoppingItemStatus
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> =
+        _insertShoppingItemStatus
 
     fun setCurrentImage(url: String) {
         _curImageUrl.postValue(url)
@@ -39,16 +41,77 @@ class ShoppingViewModel @Inject constructor(
         repository.deleteShoppingItem(shoppingItem)
     }
 
-    fun insertShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launch {
+    fun insertShoppingItemIntoDb(shoppingItem: ShoppingItem) = viewModelScope.launch {
         repository.insertShoppingItem(shoppingItem)
     }
 
 
     fun insertShoppingItem(name: String, amountString: String, priceString: String) {
+        if (name.isEmpty() || amountString.isEmpty() || priceString.isEmpty()) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The gields must not be empty",
+                        null
+                    )
+                )
+            )
+            return
+        }
 
+        if (name.length > Constants.MAX_NAME_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "Name of the item must not exceed ${Constants.MAX_NAME_LENGTH} characters",
+                        null
+                    )
+                )
+            )
+            return
+        }
+
+        if (priceString.length > Constants.MAX_PRICE_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The price od the item must not exceed ${Constants.MAX_PRICE_LENGTH} charackers",
+                        null
+                    )
+                )
+            )
+            return
+        }
+
+        val amount = try {
+            amountString.toInt()
+        } catch (e: Exception) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "Please enter a valid amount",
+                        null
+                    )
+                )
+            )
+            return
+        }
+
+        val shoppingItem =
+            ShoppingItem("name", amount, priceString.toFloat(), _curImageUrl.value ?: "")
+        insertShoppingItemIntoDb(shoppingItem)
+        setCurrentImage("")
+        _insertShoppingItemStatus.postValue(Event(Resource.success(shoppingItem)))
     }
 
     fun searchForImage(imageQuery: String) {
-
+        if (imageQuery.isEmpty()) {
+            return
+        }
+        _images.value = Event(Resource.loading(null))
+        viewModelScope.launch {
+            val response = repository.searchForImage(imageQuery)
+            _images.value = Event(response)
+        }
     }
 }
